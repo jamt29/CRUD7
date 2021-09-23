7732ba88eeb4c5ea6a4e4b84536d011077210cd9
alvitronex <marioenriquealvarado12@gmail.com>
2021-09-22 19:47:24 -0600

    Complete class consulta_recycleView and ConsultaSpinner

--- app/src/main/java/com/itca/crud7/consulta_recyclerView.java
+++ app/src/main/java/com/itca/crud7/consulta_recyclerView.java
@@ -1,6 +1,41 @@
 package com.itca.crud7;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
-import android.app.Activity;
+public class consulta_recyclerView extends AppCompatActivity {
+    private RecyclerView recycler;
+    private AdaptadorArticulos adaptadorArticulos;
+    ConexionSQLite datos = new ConexionSQLite(consulta_recyclerView.this);
 
-public class consulta_recyclerView extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+        recycler = (RecyclerView)findViewById(R.id.rview);
+        // Esta línea mejora el rendimiento, si sabemos que el contenido
+        // no va a afectar al tamaño del RecyclerView
+
+        recycler.setHasFixedSize(true);
+
+        // Nuestro RecyclerView usará un linear layout manager
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        //adaptadorArticulos = new AdaptadorArticulos(consulta_recyclerView.this, obtenerArticulos());
+        adaptadorArticulos = new AdaptadorArticulos(consulta_recyclerView.this, datos.mostrarArticulos());
+        recycler.setAdapter(adaptadorArticulos);
+    }
+
+    //Este método lo he creado para probar el funcionamiento sin obtener los registros de una base  de datos.
+    public List<Dto> obtenerArticulos() {
+        List<Dto> articulos = new ArrayList<>();
+        articulos.add(new Dto(1, "Laptop", 200.99));
+        articulos.add(new Dto(2, "Impresora HP", 100.78));
+        articulos.add(new Dto(3, "Disco Duro 1TB", 100.19));
+        return articulos;
+    }
 }
+
